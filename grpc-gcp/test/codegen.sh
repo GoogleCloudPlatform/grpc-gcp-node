601c17f7e4de245ff5fc1d127c7fda356933d98d
Weiran Fang <weiranf@google.com>
2018-10-10 17:03:48 -0700

    Change submodule path

--- grpc-gcp/test/codegen.sh
+++ grpc-gcp/test/codegen.sh
@@ -5,9 +5,9 @@ rm -rf google
 
 PROTOC=../node_modules/grpc-tools/bin/protoc.js
 
-for p in $(find ./googleapis/google -type f -name *.proto); do
+for p in $(find ../../third_party/googleapis/google -type f -name *.proto); do
   $PROTOC \
-    --proto_path=./googleapis \
+    --proto_path=../../third_party/googleapis \
     --js_out=import_style=commonjs,binary:./ \
     --grpc_out=./ \
     "$p"
