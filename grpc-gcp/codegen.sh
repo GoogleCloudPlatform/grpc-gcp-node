2fd39dad040d1ffd8ee6e4a94a0efd2911a87b01
Weiran Fang <weiranf@google.com>
2018-10-22 10:56:15 -0700

    Add README and fix build

--- grpc-gcp/codegen.sh
+++ grpc-gcp/codegen.sh
@@ -5,6 +5,11 @@ PBJS=./node_modules/protobufjs/bin/pbjs
 PBTS=./node_modules/protobufjs/bin/pbts
 OUTDIR=./src/generated
 
+rm -r $OUTDIR
 mkdir $OUTDIR
+
 $PBJS -t static-module -w commonjs -o $OUTDIR/grpc_gcp.js protos/grpc_gcp.proto
+echo "Generated src/generated/grpc_gcp.js"
+
 $PBTS -o $OUTDIR/grpc_gcp.d.ts $OUTDIR/grpc_gcp.js
+echo "Generated src/generated/grpc_gcp.d.ts"
