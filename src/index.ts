7e12a662a3a02b0ee057a69dae872fd86c4bd1fc
danieljbruce <danieljbruce@users.noreply.github.com>
2023-01-23 14:11:19 -0500

    Define a generic interface to allow flexibility (#160)
    * Define a generic interface to allow flexibility
    
    * Add intercepting call
    
    * Remove export
    
    * Use the grpc interface inside the factory
    
    * closer match to design doc

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@
  *
  */
 import * as grpcType from '@grpc/grpc-js';
+import {GrpcInterface} from './grpc_interface';
 import * as util from 'util';
 
 import {ChannelRef} from './channel_ref';
@@ -30,7 +31,8 @@ import AffinityConfig = protoRoot.grpc.gcp.AffinityConfig;
 
 type GrpcModule = typeof grpcType;
 
-export = (grpc: GrpcModule) => {
+export = (grpc: GrpcInterface) => setup(grpc as GrpcModule);
+const setup = (grpc: GrpcModule) => {
   const GcpChannelFactory = getGcpChannelFactoryClass(grpc);
   /**
    * Create ApiConfig proto message from config object.
